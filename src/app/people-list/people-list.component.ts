065918302f79520339032491e14aa5e7709ed049
Scott Stadt <scott.stadt@gmail.com>
2017-06-26 12:13:11 -0400

    steps 1 and 2

---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,26 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PeopleService } from '../people.service';
+
+@Component({
+  selector: 'app-people-list',
+  template: `
+    <ul>
+      <li *ngFor="let person of people">{{person.name}}</li>
+    </ul>
+  `,
+  styleUrls: ['./people-list.component.scss']
+})
+
+export class PeopleListComponent implements OnInit {
+
+  people: Person[] = [];
+
+  constructor(private _peopleService : PeopleService) { }
+
+  ngOnInit() {
+    this.people = this._peopleService.getAll();
+  }
+
+}
---
+++ src/app/person.ts
@@ -0,0 +1,6 @@
+
+export interface Person {
+  name: string;
+  weight: number;
+  height: number;
+}
